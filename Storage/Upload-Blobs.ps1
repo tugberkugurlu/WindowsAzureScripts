563bffd6be4c69be58c4542ca6b6ad17d7dfbcca
tugberkugurlu <tugberkugurlu@gmail.com>
2013-04-28 14:02:50 +0300

    added Upload-Blobs.ps1 to upload all files under a folder with mime type mapping

---
+++ Storage/Upload-Blobs.ps1
@@ -0,0 +1,26 @@
+﻿########################################################################################
+## Samples: 
+##          ls -File -Recurse | Set-AzureStorageBlobContent -Container upload
+##          $meta = @{"key" = "value"; "name" = "test"}
+##          Set-AzureStorageBlobContent -File filename -Container containername -Metadata $meta
+##          Get-AzureStorageContainer -Container container* | Set-AzureStorageBlobContent -File filename -Blob blobname
+## Files: 
+##          SetAzureStorageBlobContent.cs
+##          https://github.com/WindowsAzure/azure-sdk-tools/blob/master/WindowsAzurePowershell/src/Management.Storage/Blob/Cmdlet/SetAzureStorageBlobContent.cs
+## Ref: 
+##          http://blogs.msdn.com/b/webdev/archive/2013/02/14/getting-a-mime-type-from-a-file-extension-in-asp-net-4-5.aspx
+########################################################################################
+
+param(
+    [Parameter(Mandatory = $true)][String]$directoryPath,
+    [Parameter(Mandatory = $true)][String]$containerName
+)
+
+[System.Reflection.Assembly]::LoadWithPartialName("System.Web") | Out-Null
+$container = Get-AzureStorageContainer -Name $containerName -Verbose
+
+Get-ChildItem -Path $directory -File | foreach {
+    
+    $props = @{ 'ContentType' = [System.Web.MimeMapping]::GetMimeMapping($_.Name) }
+    $container | Set-AzureStorageBlobContent -Properties $props -File $_.FullName -Verbose
+}
