2d079cb6aa0a62dafae8e19f9eaefab6eea6ae20
tugberkugurlu <tugberkugurlu@gmail.com>
2014-07-11 16:48:19 +0300

    inline script all the things: http://social.msdn.microsoft.com/Forums/en-US/fb0887c3-d140-4b99-a72f-62b9d07a72e8/error-running-azurestoragefile-powershell-script?forum=azureautomation#95c92ba0-4df1-47cd-93af-d7ed96e47a40

--- Automation/Backup-BlobStorage.ps1
+++ Automation/Backup-BlobStorage.ps1
@@ -18,53 +18,56 @@ workflow Backup-BlobStorage {
     Connect-Azure -AzureConnectionName $AzureConnectionName
     Select-AzureSubscription -SubscriptionName $AzureConnectionName -Default
 
-    ## Get storage account to take the blobs from and its context
-    $storageAccountToTakeBackupFrom = Get-AzureStorageAccount -StorageAccountName $StorageAccountNameToTakeBackupFrom
-    $storageAccountToTakeBackupFromCreds = $storageAccountToTakeBackupFrom | Get-AzureStorageKey
-    Write-Output "Cred of $($StorageAccountNameToTakeBackupFrom): $($storageAccountToTakeBackupFromCreds.Primary)"
-    $storageAccountToTakeBackupFromCtx = New-AzureStorageContext `
-        -StorageAccountName $StorageAccountNameToTakeBackupFrom `
-        -StorageAccountKey $storageAccountToTakeBackupFromCreds.Primary
+    InlineScript {
+        
+        ## Get storage account to take the blobs from and its context
+        $storageAccountToTakeBackupFrom = Get-AzureStorageAccount -StorageAccountName $Using:StorageAccountNameToTakeBackupFrom
+        $storageAccountToTakeBackupFromCreds = $storageAccountToTakeBackupFrom | Get-AzureStorageKey
+        Write-Output "Cred of $($Using:StorageAccountNameToTakeBackupFrom): $($storageAccountToTakeBackupFromCreds.Primary)"
+        $storageAccountToTakeBackupFromCtx = New-AzureStorageContext `
+            -StorageAccountName $Using:StorageAccountNameToTakeBackupFrom `
+            -StorageAccountKey $storageAccountToTakeBackupFromCreds.Primary
+        
+        ## Get storage account to take the blobs to
+        $storageAccountToTakeBackupTo = Get-AzureStorageAccount -StorageAccountName $Using:StorageAccountNameToTakeBackupTo
+        $storageAccountToTakeBackupToCreds = $storageAccountToTakeBackupTo | Get-AzureStorageKey
+        Write-Output "Cred of $($Using:StorageAccountNameToTakeBackupTo): $($storageAccountToTakeBackupToCreds.Primary)"
+        $storageAccountToTakeBackupToCtx = New-AzureStorageContext `
+            -StorageAccountName $StorageAccountNameToTakeBackupTo `
+            -StorageAccountKey $storageAccountToTakeBackupToCreds.Primary
 
-    ## Get storage account to take the blobs to
-    $storageAccountToTakeBackupTo = Get-AzureStorageAccount -StorageAccountName $StorageAccountNameToTakeBackupTo
-    $storageAccountToTakeBackupToCreds = $storageAccountToTakeBackupTo | Get-AzureStorageKey
-    Write-Output "Cred of $($StorageAccountNameToTakeBackupTo): $($storageAccountToTakeBackupToCreds.Primary)"
-    $storageAccountToTakeBackupToCtx = New-AzureStorageContext `
-        -StorageAccountName $StorageAccountNameToTakeBackupTo `
-        -StorageAccountKey $storageAccountToTakeBackupToCreds.Primary
+        $storageAccountToTakeBackupFromCtx | Get-AzureStorageContainer |
 
-    $storageAccountToTakeBackupFromCtx | Get-AzureStorageContainer |
+            foreach {
 
-        foreach {
+                Write-Output "$([Environment]::NewLine)"
+                Write-Output "=========== Container: $($_.Name) ==========="
+                Write-Output "Backup started for container '$($_.Name)' on storage account '$($_.StorageAccountName)'"
 
-            Write-Output "$([Environment]::NewLine)"
-            Write-Output "=========== Container: $($_.Name) ==========="
-            Write-Output "Backup started for container '$($_.Name)' on storage account '$($_.StorageAccountName)'"
+                $blobs = $_ | Get-AzureStorageBlob -Context $storageAccountToTakeBackupFromCtx -MaxCount 10
+                $cToken = ($blobs | select -Last 1).ContinuationToken
 
-            $blobs = $_ | Get-AzureStorageBlob -Context $storageAccountToTakeBackupFromCtx -MaxCount 10
-            $cToken = ($blobs | select -Last 1).ContinuationToken
-
-            Write-Output "Retrieved $($blobs.Length) blobs from container '$($_.Name)' on storage account '$($_.StorageAccountName)'"
-
-            ## process the first set of blobs here...
-            foreach($blob in $blobs)
-            {
-                Write-Output "Found blob '$($blob.Name)' inside container '$($_.Name)' on storage account $($_.StorageAccountName). Backup started for this blob."
-            }
-
-            while($cToken -ne $null) {
-
-                $nextBlobs = $_ | Get-AzureStorageBlob -Context $storageAccountToTakeBackupFromCtx -MaxCount 10 -ContinuationToken $cToken
-                $cToken = ($nextBlobs | select -Last 1).ContinuationToken
-                
-                Write-Output "Retrieved next $($nextBlobs.Length) blobs from container '$($_.Name)' on storage account '$($_.StorageAccountName)'"
+                Write-Output "Retrieved $($blobs.Length) blobs from container '$($_.Name)' on storage account '$($_.StorageAccountName)'"
 
                 ## process the first set of blobs here...
                 foreach($blob in $blobs)
                 {
                     Write-Output "Found blob '$($blob.Name)' inside container '$($_.Name)' on storage account $($_.StorageAccountName). Backup started for this blob."
                 }
+
+                while($cToken -ne $null) {
+
+                    $nextBlobs = $_ | Get-AzureStorageBlob -Context $storageAccountToTakeBackupFromCtx -MaxCount 10 -ContinuationToken $cToken
+                    $cToken = ($nextBlobs | select -Last 1).ContinuationToken
+                
+                    Write-Output "Retrieved next $($nextBlobs.Length) blobs from container '$($_.Name)' on storage account '$($_.StorageAccountName)'"
+
+                    ## process the first set of blobs here...
+                    foreach($blob in $blobs)
+                    {
+                        Write-Output "Found blob '$($blob.Name)' inside container '$($_.Name)' on storage account $($_.StorageAccountName). Backup started for this blob."
+                    }
+                }
             }
-        }
+    }
 }
